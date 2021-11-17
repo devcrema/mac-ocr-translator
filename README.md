6b0c220753bb8430f1ce6f24620781fe1e64b7a7
devcrema <devcrema@gmail.com>
2021-11-17 22:35:31 +0900

    :goal_net: add catch shortcut key event

--- README.md
+++ README.md
@@ -4,10 +4,9 @@
 - preference - 손쉬운 사용 - + pycharm 혹은 해당 프로그램 등록
 
 ### TODO
-- [ ] python key event listener for screenshot
-- [ ] python running in background and launch at login
-- [ ] python key event that trigger screenshot
-- [ ] ocr
-- [ ] translator
+- [x] python key event listener for screenshot
+- [ ] python key event that trigger screenshot (https://github.com/ianzhao05/textshot)
+- [ ] translator (https://github.com/ssut/py-googletrans)
 - [ ] overlay result
+- [ ] python running in background and launch at login
 - [ ] close result by key event
--- keyevent/key_event_listener.py
+++ keyevent/key_event_listener.py
@@ -1,15 +1,33 @@
 from pynput import keyboard
+from pynput.keyboard import Key
+
+shortcut_combination = {Key.cmd: False, Key.shift: False, "1": False}
+
+
+def check_shortcut(key: Key, pressed: bool) -> bool:
+    try:
+        if key.char == "1":
+            shortcut_combination["1"] = pressed
+    except AttributeError:
+        if key == Key.cmd:
+            shortcut_combination[Key.cmd] = pressed
+        if key == Key.shift:
+            shortcut_combination[Key.shift] = pressed
+    for result in shortcut_combination.values():
+        if not result:
+            return False
+    return True
 
 
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
+    shortcut_pressed = check_shortcut(key, True)
+    print("shortcut pressed")
+    print(shortcut_pressed)
 
 
 def on_release(key):
-    print('{0} released'.format(key))
+    check_shortcut(key, False)
+
     if key == keyboard.Key.esc:
         # Stop listener
         return False
@@ -20,8 +38,3 @@ def listen():
             on_press=on_press,
             on_release=on_release) as listener:
         listener.join()
-    # ...or, in a non-blocking fashion:
-    # listener = keyboard.Listener(
-    #     on_press=on_press,
-    #     on_release=on_release)
-    # listener.start()
