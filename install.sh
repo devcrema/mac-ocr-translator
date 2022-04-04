42c1005cf8358a2dab633477f2ab4a5a2cd7fd17
sungeun-park <sungeun.park@toss.im>
2022-04-04 12:36:35 +0900

    :hammer: update launch script

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p ~/.mac-ocr-translator
-cd ~/.mac-ocr-translator
-git clone https://github.com/devcrema/mac-ocr-translator.git ./src
-# TODO register launch at login
--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 ~/.mac-ocr-translator/src/main.py
+python3 ./main.py
